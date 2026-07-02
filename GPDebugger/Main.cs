2ae928bc560cc7c4de50813cf2ae2090be510e82
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2026-07-02 11:59:11 +0900

    v1.0.7

--- GPDebugger/Main.cs
+++ GPDebugger/Main.cs
@@ -9,7 +9,7 @@ namespace GPDebugger
     {
         public override string Name => "GPDebugger";
         public override string Author => "GoldenPig1205";
-        public override Version Version { get; } = new(1, 0, 6);
+        public override Version Version { get; } = new(1, 0, 7);
         public override Version RequiredExiledVersion { get; } = new Version(9, 13, 2);
 
         public static Main Instance { get; set; }
